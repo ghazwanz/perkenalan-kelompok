edad4820e7aec76f85944e2aa324a72ba6d2128a
Ghazwan Ababil <ababilghazwan@gmail.com>
2024-09-06 11:21:00 +0700

    perkenalan ghazwan

---
+++ perkenalanAbil.java
@@ -0,0 +1,19 @@
+/**
+ * perkenalan-ghazwan
+ */
+public class perkenalanAbil {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Ghazwan Ababil");
+        System.out.println("NIM     : 244107020151");
+        System.out.println("Kelas   : 1E Teknik Informatika");
+        System.out.println("No Absen: 11");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Jalan Raya Gadang No.421");
+        System.out.println("No HP   : 087886424760");
+        System.out.println("Hobi    : Trace Framer");
+
+    }
+}
