be83a6ab21a954e4c974283815b3208cc4919ac7
fitraadhim <azzazria123@gmail.com>
2024-09-06 11:01:22 +0700

    Membuat Perkenalan Diri

---
+++ PerkenalanFitraAdhim.java
@@ -0,0 +1,15 @@
+public class PerkenalanFitraAdhim {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--DATA MAHASISWA JURUSAN TEKNOLOGI INFORMASI POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama      :Muhammad Fitra Adhim Nurrochman");
+        System.out.println("Nim       :244107020089");
+        System.out.println("Kelas     :TI-1E");
+        System.out.println("No Absen  :19");
+        System.out.println("Prodi     :D-IV Teknik Informatika");
+        System.out.println("Alamat    :Jl. Inspeksi Brantas No.29B, Mojoroto, Mojoroto, Kota Kediri");
+        System.out.println("No Hp     :085232904562");
+        System.out.println("Hobi      :Olahraga");
+    }
+}
